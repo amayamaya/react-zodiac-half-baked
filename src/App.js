81c226c3b40cd56784d0b8d0c6a35cafb1347e8c
‘AMAYA <aamaya547@gmail.com>
2022-06-06 16:23:54 -0700

    components appearing with css classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-{/* import the zodiac array */}
-
+{
+  /* import the zodiac array */
+}
+import Header from './Header/Header.js';
+import Main from './Main/Main';
+import Footer from './Footer/Footer.js';
+import './App.css';
+import './Main/Main.css';
+import ZodiacCard from './ZodiacCard/ZodiacCard';
+('./ZodiacCard.css');
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
       {/* import and use the Header. Main, and Footer components here */}
+
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
